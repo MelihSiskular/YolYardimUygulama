bc9e47a97b4750ada4a92619821e56c02b6507ce
Melih Şişkular <feis_melih@hotmail.com>
2025-04-18 21:14:40 +0300

    Initial Commit

---
+++ YolYardimUygulamaProje/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YolYardimUygulamaProje
+//
+//  Created by Melih Şişkular on 18.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YolYardimUygulamaProje/YolYardimUygulamaProjeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YolYardimUygulamaProjeApp.swift
+//  YolYardimUygulamaProje
+//
+//  Created by Melih Şişkular on 18.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct YolYardimUygulamaProjeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
