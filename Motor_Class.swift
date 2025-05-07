16f1e6a2ae7bcb3db36e7a617e1ca39fed035219
ZengnAhmetcn <ahmetcan.bjk.zengin@gmail.com>
2025-05-07 13:28:01 +0300

    Add files via upload

---
+++ Motor_Class.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class Motor:Vasita{
+    var motormarka: MotorMarka
+
+    init(motormarka: MotorMarka,arac: AracTuru, yil:Int){
+        self.motormarka
+
+        super.init(arac: arac, yil:yil)
+    }
+}
+
+enum MotorMarka{
+    case Arora, Bajaj, CFmoto, Hondamoto, Kuba, Mondial, RKS, SYM, TVS, Vespa, Yamaha
+}
